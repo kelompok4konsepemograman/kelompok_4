54f28306a1711d25ec8a39bd3c6555324135c96c
Army <44823977+armymen28@users.noreply.github.com>
2018-12-15 19:21:23 +0700

    Update game2.py

--- game2.py
+++ game2.py
@@ -54,4 +54,4 @@ button8 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, comma
 button8.grid(row=3, column=1,sticky = S+N+E+W)
 button9 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button9))
 button9.grid(row=3, column=2,sticky = S+N+E+W)
-tk.mainloop()"# game_of_hand" 
+tk.mainloop()
